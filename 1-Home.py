4115aba463bbc05bcaf8ad17bfa40f2bb2a661ce
WAKenney <80290200+WAKenney@users.noreply.github.com>
2024-03-13 17:42:03 +0000

    time 1:40 pm MArch 13

--- 1-Home.py
+++ 1-Home.py
@@ -16,7 +16,7 @@ titleCol1, titleCol2, titleCol3 =st.columns((1,4,1))
 title = 'nw4_splash_page.png'
 
 # Print the date and time of the last commit
-st.write("This app was last updated on March 13 2024 11:16 am")
+st.write("This app was last updated on March 13 2024 1:40 pm")
 
 titleCol2.image(title)
 
